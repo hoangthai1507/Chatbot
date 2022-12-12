06d8511806251bbb1abdf60bc17663692c954a82
huyhoangg <huyhoang1472@gmail.com>
2022-12-12 15:25:20 +0700

    new action and db

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,38 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from bs4 import BeautifulSoup
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from database import getdata
+import requests
+
+
+class ActionFuelPrice(Action):
+
+    def name(self) -> Text:
+        return "action_fuel_price"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+
+        price = getdata.getdata(0, "gas")
+        dispatcher.utter_message(text=f"{price}")
+   
+        
+class ActionGold(Action):
+
+    def name(self) -> Text:
+        return "action_jenewry_price"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+
+        
+        dispatcher.utter_message(text=f"vang 5 trieu")
